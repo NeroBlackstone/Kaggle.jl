c890f246aea031608bce0bc2672259d87d4a1f84
NeroBlackstone <gf7600gs@gmail.com>
2023-07-25 18:26:40 +0800

    try fetch file

--- src/Kaggle.jl
+++ src/Kaggle.jl
@@ -1,14 +1,22 @@
 module Kaggle
 
-imp
-using JSON
+include("APIClient.jl")
 
+using JSON
+using .APIClient
+using Base64
 
 function parse_kaggle_json()
     file = JSON.parsefile(joinpath("$(homedir())/.kaggle/kaggle.json"))
     return file["username"],file["key"]
 end
 
-# @show parse_kaggle_json()
+user,key = parse_kaggle_json()
+basic_auth = base64encode("$(user):$(key)")
 
+client = APIClient.Clients.Client(APIClient.basepath(APIClient.KaggleApi);headers=Dict("Authorization"=>"Basic $(basic_auth)"))
+api = APIClient.KaggleApi(client)
+
+# display(datasets_list(api)[2])
+# display(datasets_download(api,"kapturovalexander","students-in-rooms")[2])
 end # module Kaggle
